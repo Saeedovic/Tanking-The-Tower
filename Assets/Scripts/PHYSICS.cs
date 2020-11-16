6bed2dbb8601d75b537c30b7c782e62c0eb9ff51
Ibrat <muminov.ibrat@gmail.com>
2020-11-16 20:33:12 +0400

    Made tank spin, shoot, have recoil and camera follow player.

--- Assets/Scripts/PHYSICS.cs
+++ Assets/Scripts/PHYSICS.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class PHYSICS : MonoBehaviour
 {
- 
     //Drag in the Bullet Emitter from the Component Inspector.
     public GameObject Bullet_Emitter;
 
     //Drag in the Bullet Prefab from the Component Inspector.
     public GameObject Bullet;
+    public int  spinspeed;
+    public int  fastspin;
+    public Rigidbody rb;
+    public float forceMult;
 
     //Enter the Speed of the Bullet from the Component Inspector.
     public float Bullet_Forward_Force;
@@ -17,13 +20,13 @@ public class PHYSICS : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             //The Bullet instantiation happens here.
             GameObject Temporary_Bullet_Handler;
@@ -42,6 +45,16 @@ public class PHYSICS : MonoBehaviour
 
             //Basic Clean Up, set the Bullets to self destruct after 10 Seconds, I am being VERY generous here, normally 3 seconds is plenty.
             Destroy(Temporary_Bullet_Handler, 10.0f);
+
+            rb.velocity = transform.forward * Time.deltaTime * -forceMult;
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+        transform.Rotate(0, fastspin, 0 * Time.deltaTime);
+        }
+        else
+        {
+        transform.Rotate(0, spinspeed, 0 * Time.deltaTime);
         }
     }
 }
---
+++ Assets/Scripts/studying.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class studying : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forceMult;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+
+        }
+    }
+
+    void Launch()
+    {
+        rb.velocity = transform.forward * Time.deltaTime * -forceMult;
+    }
+}
