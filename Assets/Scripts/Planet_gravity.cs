0f40d76f0036ce59eacf6b9dc7f8949322a010b7
INotAntony <antonio.garcia@student.saedubai.com>
2020-11-28 11:44:43 +0400

    changed all objects to 2D physics and added gravity to all planets as well as added a bounce mechanic on the tank

---
+++ Assets/Scripts/Planet_gravity.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet_gravity : MonoBehaviour
+{
+    public List<GameObject> gravitylist;
+    public float gravityScale;
+    Vector2 dir;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < gravitylist.Count; i++)
+        {
+            dir = (gravitylist[i].transform.position - this.transform.position).normalized * gravityScale;
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        for (int i = 0; i < gravitylist.Count; i++)
+        {
+            gravitylist[i].transform.position = Vector2.MoveTowards(gravitylist[i].transform.position, this.transform.position, gravityScale);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        gravitylist.Add(collision.gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        gravitylist.Remove(collision.gameObject);
+
+    }
+}
